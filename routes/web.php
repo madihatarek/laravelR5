ce01981e8879b798ea3d3c42775fd205a246b8b7
madihatarek <madihatarek6112000@gmail.com>
2024-04-24 21:46:50 +0200

    main

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('madiha/{id}', function($id){
+    return "welocme to my website ". $id;
+});
+//Route ===> class
